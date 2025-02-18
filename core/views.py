06ddef0f23c87a75d963bdf45d1c8f5e515adc9a
Hsu-Pei-Chun <philsophysis@gmail.com>
2025-02-18 22:46:38 +0800

    chore: update Dockerfile, improve README, and adjust auth UI/routes

--- core/views.py
+++ core/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, RedirectView
+from django.urls import reverse_lazy
 
-def home(request):
-    return render(request, 'core/index.html')
+# 首頁
+class HomeView(TemplateView):
+    template_name = 'core/index.html'
+
+# 登入頁面
+class LoginView(TemplateView):
+    template_name = 'core/login.html'
+
+# 註冊頁面
+class RegisterView(TemplateView):
+    template_name = 'core/register.html'
+
+# 登出後跳轉首頁
+class LogoutView(RedirectView):
+    url = reverse_lazy('home')
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RegisterView, LoginView, LogoutView
 
+app_name = 'users_api'
+
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
